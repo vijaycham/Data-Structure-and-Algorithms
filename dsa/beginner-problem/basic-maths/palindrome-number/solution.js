51cbe4d756a9eb4b8c54319c5b41eb1cedecdee1
vijaycham <vijaysivakadatcham@gmail.com>
2025-01-14 22:39:24 +0530

    Solved:
    Success: true
    Test Cases: 14
    Time: 0.060ms
    Memory: 7260.00KB

---
+++ dsa/beginner-problem/basic-maths/palindrome-number/solution.js
@@ -0,0 +1,31 @@
+/*
+Problem: 
+Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-maths/palindrome-number?tab=submissions
+
+Description:
+
+
+Stats:
+- Success: true
+- Test Cases: 14
+- Time: 0.060ms
+- Memory: 7260.00KB
+*/
+
+class Solution {
+    isPalindrome(n) {
+
+        let copy = n;
+
+        let revNum=0;
+
+        while(n>0) {
+            let lastDigit = n% 10;
+
+            revNum = (revNum*10) + lastDigit;
+            n = Math.floor(n/10);
+
+        }
+         return revNum === copy;
+    }
+}
