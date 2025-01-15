0cd384d9ae726fde2276238f77d3f5685abd6d1c
vijaycham <vijaysivakadatcham@gmail.com>
2025-01-15 20:44:32 +0530

    Solved:
    Success: true
    Test Cases: 112
    Time: 0.069ms
    Memory: 8082.00KB

--- dsa/beginner-problem/basic-strings/reverse-a-string/solution.js
+++ dsa/beginner-problem/basic-strings/reverse-a-string/solution.js
@@ -1,31 +1,26 @@
 /*
-Problem: Palindrome number
+Problem: 
 Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-strings/reverse-a-string?tab=submissions
 
 Description:
-You are given an integer n. You need to check whether the number is a palindrome number or not. Return true if it's a palindrome number, otherwise return false.A palindrome number is a number which reads the same both left to right and right to left.
+
 
 Stats:
 - Success: true
 - Test Cases: 112
-- Time: 0.066ms
-- Memory: 8024.00KB
+- Time: 0.069ms
+- Memory: 8082.00KB
 */
 
 class Solution {
-    isPalindrome(n) {
+    reverseString(s) {
+        let i =0 ;
+        let j = s.length -1;
 
-        let copy = n;
-
-        let revNum=0;
-
-        while(n>0) {
-            let lastDigit = n% 10;
-
-            revNum = (revNum*10) + lastDigit;
-            n = Math.floor(n/10);
-
-        }
-         return revNum === copy;
+        while (i < j) {
+            [s[i], s[j]] = [s[j], s[i]];
+            i++,
+            j--;
+        } return s ;
     }
 }
