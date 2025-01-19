6e0bbb51355b358ea621ac3c6cc93a2f97a2c160
vijaycham <vijaysivakadatcham@gmail.com>
2025-01-19 22:38:28 +0530

    Solved: Return the largest digit in a number
    Success: true
    Test Cases: 117
    Time: 0.046ms
    Memory: 7520.00KB

---
+++ dsa/beginner-problem/basic-strings/largest-odd-number-in-a-string/solution.js
@@ -0,0 +1,29 @@
+/*
+Problem: Return the largest digit in a number
+Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-strings/largest-odd-number-in-a-string?tab=submissions
+
+Description:
+You are given an integer n. Return the largest digit present in the number.
+
+Stats:
+- Success: true
+- Test Cases: 117
+- Time: 0.046ms
+- Memory: 7520.00KB
+*/
+
+class Solution {
+    largestDigit(n) {
+             let largestDigit = 0;
+        while (n > 0) {
+            let lastDigit = n % 10;
+            if (lastDigit > largestDigit) {
+                largestDigit = lastDigit;
+            }
+
+            n = Math.floor(n / 10);
+        }
+
+        return largestDigit;
+    }
+}
