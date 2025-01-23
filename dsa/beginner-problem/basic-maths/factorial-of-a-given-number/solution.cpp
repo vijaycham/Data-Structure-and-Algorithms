400059b9543acf7d94eda912a50715581cf8d3e0
vijaycham <vijaysivakadatcham@gmail.com>
2025-01-23 22:47:47 +0530

    Solved:
    Success: true
    Test Cases: 13
    Time: 0.064ms
    Memory: 7540.00KB

---
+++ dsa/beginner-problem/basic-maths/factorial-of-a-given-number/solution.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: 
+Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-maths/factorial-of-a-given-number?tab=submissions
+
+Description:
+
+
+Stats:
+- Success: true
+- Test Cases: 13
+- Time: 0.064ms
+- Memory: 7540.00KB
+*/
+
+class Solution {
+public:
+    int factorial( n) {
+        let fact = 1;
+
+        for(let i = 1; i<n; i++) {
+            fact = fact * i
+        }
+        return fact
+    }
+};
