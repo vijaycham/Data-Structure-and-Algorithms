fe94da4952a4a2f5d7bbfd43e5690fc513aed8a5
vijaycham <vijaysivakadatcham@gmail.com>
2025-01-30 22:01:27 +0530

    Solved: Pattern 14
    Success: true
    Test Cases: 22
    Time: 0.063ms
    Memory: 9300.00KB

---
+++ dsa/beginner-problem/patterns/pattern-14/solution.js
@@ -0,0 +1,28 @@
+/*
+Problem: Pattern 14
+Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/patterns/pattern-14?tab=submissions
+
+Description:
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:AABABCABCDABCDEPrint the pattern in the function given to you.
+
+Stats:
+- Success: true
+- Test Cases: 22
+- Time: 0.063ms
+- Memory: 9300.00KB
+*/
+
+class Solution {
+    pattern14(n) {
+         for (let i = 0; i < n; i++) {
+            let ch = 'A';
+
+            for (let j = 0; j <= i; j++) {
+                process.stdout.write(ch);
+                ch = String.fromCharCode(ch.charCodeAt(0) + 1);
+            }
+            console.log();
+        }
+
+    }
+}
