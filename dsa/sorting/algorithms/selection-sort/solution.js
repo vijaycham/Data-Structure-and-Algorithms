cd787f59ab11daa01f1dd21ee3c9306e8f4923df
VIJAY S <vijaysivakadatcham@gmail.com>
2025-03-23 21:04:14 +0530

    Solved: Bubble Sort
    Problem Link: https://takeuforward.org/plus/dsa/sorting/algorithms/selection-sort?tab=submissions
    
    Description:
    Given an array of integers called nums,sort the array in non-decreasing order using the bubble sort algorithm and return the sorted array.A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
    
    Stats:
    - Success: true
    - Test Cases: 105
    - Time: 0.801ms
    - Memory: 49818.00KB

---
+++ dsa/sorting/algorithms/selection-sort/solution.js
@@ -0,0 +1,13 @@
+// Solution for: Bubble Sort
+class Solution {
+    bubbleSort(nums) {
+              let n = nums.length
+              for(let i = 0; i < n ; i++) {
+                for(let j = 0 ; j < n-i-1; j++) {
+                    if(nums[j] > nums[j+1]) {
+                        [nums[j],nums[j+1]] = [nums[j+1], nums[j]]
+                    }
+                }
+              } return nums
+    }  
+}
