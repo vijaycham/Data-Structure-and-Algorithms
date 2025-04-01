62109ccf02f427e8c164d890280e0cc2081f872a
VIJAY S <vijaysivakadatcham@gmail.com>
2025-04-01 12:43:43 +0530

    Solved: Count of odd numbers in array
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-arrays/reverse-an-array?tab=submissions
    
    Description:
    Given an array of n elements. The task is to return the count of the number of odd numbers in the array.
    
    Stats:
    - Success: true
    - Test Cases: 106
    - Time: 0.815ms
    - Memory: 49472.00KB

---
+++ dsa/beginner-problem/basic-arrays/reverse-an-array/solution.js
@@ -0,0 +1,11 @@
+// Solution for: Count of odd numbers in array
+class Solution {
+    countOdd(arr, n) {
+        let count = 0
+        for(let i=0 ; i < arr.length; i++ ) {
+            if(arr[i] % 2 !== 0) {
+                count++
+            } 
+        } return count
+    }
+}
