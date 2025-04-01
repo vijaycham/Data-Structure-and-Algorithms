6ea0f04451be05d6c40c22b503eb2fa0992ee9b9
VIJAY S <vijaysivakadatcham@gmail.com>
2025-04-01 12:02:37 +0530

    Solved: Count of odd numbers in array
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-arrays/count-of-odd-numbers-in-array?tab=submissions
    
    Description:
    Given an array of n elements. The task is to return the count of the number of odd numbers in the array.
    
    Stats:
    - Success: true
    - Test Cases: 106
    - Time: 0.804ms
    - Memory: 49760.00KB

---
+++ dsa/beginner-problem/basic-arrays/count-of-odd-numbers-in-array/solution.js
@@ -0,0 +1,9 @@
+// Solution for: Count of odd numbers in array
+class Solution {
+    sum(arr,n) {
+       let sum = 0
+       for(let i = 0 ; i < n; i++) {
+          sum = sum + arr[i]
+       } return sum
+    }
+}
