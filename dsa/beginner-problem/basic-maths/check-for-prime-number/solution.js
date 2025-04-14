165ec6c9b4838c752b5e58e8447b671f3518ecbe
VIJAY S <vijaysivakadatcham@gmail.com>
2025-04-14 11:40:47 +0530

    Solved: Sum of array elements
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-maths/check-for-prime-number?tab=submissions
    
    Description:
    Learn more and start your upgrade today!
    
    Stats:
    - Success: true
    - Test Cases: 13
    - Time: 0.966ms
    - Memory: 50760.00KB

---
+++ dsa/beginner-problem/basic-maths/check-for-prime-number/solution.js
@@ -0,0 +1,15 @@
+// Solution for: Sum of array elements
+class Solution {
+    arraySum(nums) {
+       
+       return this.sum(nums,0);
+}
+    sum(nums, i) {
+        if( i >= nums.length) {
+            return 0;
+        }
+
+        return nums[i] + this.sum(nums, i+1)
+    }
+    
+}
