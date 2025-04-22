2cc6ce077d51109662c5f2f3ade8985d320145d3
VIJAY S <vijaysivakadatcham@gmail.com>
2025-04-22 10:22:52 +0530

    Solved: Check if a Number is Prime or Not
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-recursion/reverse-an-array-ii?tab=submissions
    
    Description:
    Learn more and start your upgrade today!
    
    Stats:
    - Success: true
    - Test Cases: 133
    - Time: 0.724ms
    - Memory: 48632.00KB

---
+++ dsa/beginner-problem/basic-recursion/reverse-an-array-ii/solution.js
@@ -0,0 +1,15 @@
+// Solution for: Check if a Number is Prime or Not
+class Solution {
+    checkPrime(num) {
+     if(num <=1) return false
+           return this.prime(2, num)
+    }
+
+    prime(i, num) {
+        if(i*i >= num ) return true
+
+        if(num %i === 0) return false 
+
+        return this.prime(i+1, num)
+    }
+}
