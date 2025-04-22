aeac262e4447f9bacc628739bb351f1e7bc471d6
VIJAY S <vijaysivakadatcham@gmail.com>
2025-04-22 10:58:51 +0530

    Solved: Data, Information & Database
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-recursion/check-if-the-array-is-sorted-ii?tab=submissions
    
    Description:
    Learn more and start your upgrade today!
    
    Stats:
    - Success: true
    - Test Cases: 242
    - Time: 1.140ms
    - Memory: 47878.00KB

---
+++ dsa/beginner-problem/basic-recursion/check-if-the-array-is-sorted-ii/solution.js
@@ -0,0 +1,15 @@
+// Solution for: Data, Information & Database
+class Solution {
+    checkPrime(num) {
+     if(num <=1) return false
+           return this.prime(2, num)
+    }
+
+    prime(i, num) {
+        if(i*i >= num ) return true
+
+        if(num %i === 0) return false 
+
+        return this.prime(i+1, num)
+    }
+}
