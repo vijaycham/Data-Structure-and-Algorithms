478d8aeab89dd584ad963e52ff9c50c8ad41a41d
VIJAY S <vijaysivakadatcham@gmail.com>
2025-07-24 01:21:45 +0530

    Solved: Traversal in Linked List
    Problem Link: https://takeuforward.org/plus/dsa/linked-list/fundamentals-single-ll/traversal-in-linked-list?tab=submissions
    
    Description:
    Given the head of a singly Linked List. Traverse the entire Linked List and return its elements in an array in the order of their appearance.
    
    Stats:
    - Success: true
    - Test Cases: 106
    - Time: 1.036ms
    - Memory: 52658.00KB

---
+++ dsa/linked-list/fundamentals-single-ll/traversal-in-linked-list/solution.js
@@ -0,0 +1,21 @@
+// Solution for: Traversal in Linked List
+/*Definition of singly linked list:
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
+
+class Solution {
+    LLTraversal(head) {
+        let arr = []
+        while(head) {
+          arr.push(head.val)
+          head = head.next
+          
+        }
+       return arr
+    }
+}
