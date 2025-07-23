9d6728e5c174026e636696ec2e92f24fee60ef03
VIJAY S <vijaysivakadatcham@gmail.com>
2025-07-24 01:30:57 +0530

    Solved: Deletion of the head of LL
    Problem Link: https://takeuforward.org/plus/dsa/linked-list/fundamentals-single-ll/deletion-of-the-head-of-ll?tab=submissions
    
    Description:
    Given the head of a singly linked list, delete the head of the linked list and return the head of the modified list.The head is the first node of the linked list.Please note that this section might seem a bit difficult without prior knowledge on what linkedlist is, we will soon try to add basics concepts for your ease! If you know the concepts already please go ahead to give a shot to the problem. Cheers!
    
    Stats:
    - Success: true
    - Test Cases: 15
    - Time: 1.015ms
    - Memory: 52960.00KB

---
+++ dsa/linked-list/fundamentals-single-ll/deletion-of-the-head-of-ll/solution.js
@@ -0,0 +1,20 @@
+// Solution for: Deletion of the head of LL
+/*Definiton of singly Linked List
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
+
+class Solution {
+    deleteHead(head) {
+      if(!head) return head;
+      
+      let temp = head;
+      head = head.next;
+      temp = null;
+      return head
+    } 
+}
