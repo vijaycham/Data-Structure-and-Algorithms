4d489e1c94a06eba1c6f6d93297a737513f5ccd7
VIJAY S <vijaysivakadatcham@gmail.com>
2025-07-24 01:42:16 +0530

    Solved: Deletion of the tail of LL
    Problem Link: https://takeuforward.org/plus/dsa/linked-list/fundamentals-single-ll/deletion-of-the-tail-of-ll?tab=submissions
    
    Description:
    Given the head of a singly linked list, delete the tail of the linked list and return the head of the modified list.The tail is the last node of the linked list.
    
    Stats:
    - Success: true
    - Test Cases: 15
    - Time: 1.017ms
    - Memory: 52740.00KB

---
+++ dsa/linked-list/fundamentals-single-ll/deletion-of-the-tail-of-ll/solution.js
@@ -0,0 +1,21 @@
+// Solution for: Deletion of the tail of LL
+/*Definiton of singly Linked List
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
+
+class Solution {
+  deleteTail(head) {
+    if (!head || !head.next) return null;
+    let temp = head;
+    while (temp.next.next) {
+      temp = temp.next;
+    }
+    temp.next = null;
+    return head;
+  }
+}
