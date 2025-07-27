a150a72aca57b8fe2e8e154f9dc56b0f577e9b4e
VIJAY S <vijaysivakadatcham@gmail.com>
2025-07-27 23:01:50 +0530

    Time: 1 ms (99.81%) | Memory: 57.3 MB (80.28%) - LeetSync

--- Leetcode/Two-Pointers/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ Leetcode/Two-Pointers/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let n = s.length
+    let n = s.length;
+    let l = 0,
+        r = 0,
+        maxLen = 0;
+    let hash = new Array(256).fill(-1);
 
-    let maxLen = 0
-    for (let i = 0; i < n; i++) {
-        let hash = new Array(256).fill(0)
-
-        for (let j = i; j < n; j++) {
-
-            if (hash[s.charCodeAt(j)] === 1) break;
-
-            hash[s.charCodeAt(j)] = 1
-
-            let len = j - i + 1
-
-            maxLen = Math.max(len, maxLen)
+    while (r < n) {
+        if (hash[s.charCodeAt(r)] != -1) {
+            l = Math.max(hash[s.charCodeAt(r)] + 1, l);
         }
+
+        let len = r - l + 1;
+
+        maxLen = Math.max(len, maxLen);
+        hash[s.charCodeAt(r)] = r;
+        r++;
     }
-    return maxLen
-};
+    return maxLen;
+}
