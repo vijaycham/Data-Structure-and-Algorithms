73e466e364772fe64699be82cf90ff248a5fdda9
VIJAY S <vijaysivakadatcham@gmail.com>
2025-08-04 02:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (51.19%) - LeetSync

--- Leetcode/Binary-Tree/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ Leetcode/Binary-Tree/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -11,18 +11,23 @@
  * @return {number[]}
  */
 var postorderTraversal = function (root) {
-    if(!root) return []
-    let s1 = [root]
-    let s2 = []
-    while (s1.length) {
-        let curr = s1.pop()
-        s2.push(curr)
-        curr.left && s1.push(curr.left)
-        curr.right && s1.push(curr.right)
-    }
-     let ans = []
-    while(s2.length) {
-        ans.push(s2.pop().val)
-    }
-    return ans
+    let curr = root
+    let stack = []
+    let ans = []
+    let visitedNode = null
+
+    while (stack.length || curr) {
+        while (curr) {
+            stack.push(curr)
+            curr = curr.left
+        }
+        let peekNode = stack[stack.length - 1]
+        if (peekNode.right && peekNode.right != visitedNode) {
+            curr = peekNode.right
+        } else {
+            ans.push(peekNode.val)
+            visitedNode = stack.pop()
+        }
+    } return ans
+
 }
