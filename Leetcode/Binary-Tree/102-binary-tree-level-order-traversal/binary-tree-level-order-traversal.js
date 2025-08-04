3227a305628e95da9e8adf884ac43e3a7e79ea0e
VIJAY S <vijaysivakadatcham@gmail.com>
2025-08-04 23:29:14 +0530

    Time: 1 ms (85.50%) | Memory: 57.2 MB (88.44%) - LeetSync

---
+++ Leetcode/Binary-Tree/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,23 @@
+var levelOrder = function(root) {
+    if (!root) return [];
+
+    let queue = [root];
+    let result = [];
+
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let level = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+
+    return result;
+};
