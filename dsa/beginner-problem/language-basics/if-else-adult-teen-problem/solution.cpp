04d1493978f48712f33c3d3d4ac340cebc36139f
VIJAY S <vijaysivakadatcham@gmail.com>
2025-08-07 06:35:11 +0530

    Solved: If Else Adult Teen Problem
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/language-basics/if-else-adult-teen-problem?tab=submissions
    
    Description:
    Given an integer age, print on the screen:Adult if age >= 18Teen if age < 18Do not change the case of any letter in "Adult" and "Teen" while printing the answer.For printing use:-for C++ : cout << variable_name<<endl;for Java : System.out.println();for Python : print()for Javascript : console.log()
    
    Stats:
    - Success: true
    - Test Cases: 22
    - Time: 0.003ms
    - Memory: 1808.00KB

---
+++ dsa/beginner-problem/language-basics/if-else-adult-teen-problem/solution.cpp
@@ -0,0 +1,12 @@
+// Solution for: If Else Adult Teen Problem
+class Solution {
+public:
+    void isAdult(int age) {
+      
+       if(age >= 18) {
+        cout << "Adult\n";
+       } else {
+        cout << "Teen\n";
+       }
+    }
+};
