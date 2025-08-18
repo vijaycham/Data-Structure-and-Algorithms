161ed427b8591f5a534cdcb9a7c49f1133d9fbbc
VIJAY S <vijaysivakadatcham@gmail.com>
2025-08-19 00:33:43 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/binary-search-trees/theory-and-basics/search-in-bst?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 115
    - Time: 1.071ms
    - Memory: 52132.00KB

---
+++ dsa/binary-search-trees/theory-and-basics/search-in-bst/solution.js
@@ -0,0 +1,20 @@
+// Solution for: 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *      constructor(val = 0, left = null, right = null){
+ *          this.data = val;
+ *          this.left = null;
+ *          this.right = null;
+ *      }
+ * }
+ **/
+
+class Solution {
+    searchBST(root, val) {
+        while(root !== null && root.data !== val) {
+            root = (root.data > val) ? root.left : root.right
+        }
+        return root 
+    }
+}
