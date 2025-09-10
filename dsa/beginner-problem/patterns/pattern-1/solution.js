30d2c016de8fac8250df3befe07496a969c85dca
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-10 08:37:34 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/patterns/pattern-1?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 102
    - Time: 1.715ms
    - Memory: 52272.00KB

---
+++ dsa/beginner-problem/patterns/pattern-1/solution.js
@@ -0,0 +1,30 @@
+// Solution for: 
+class Solution {
+
+ studentGrade(marks) {
+
+  if (marks >= 90) {
+
+   console.log("Grade A");
+
+  } else if (marks >= 70) {
+
+   console.log("Grade B");
+
+  } else if (marks >= 50) {
+
+   console.log("Grade C");
+
+  } else if (marks >= 35) {
+
+   console.log("Grade D");
+
+  } else {
+
+   console.log("Fail");
+
+  }
+
+ }
+
+}
