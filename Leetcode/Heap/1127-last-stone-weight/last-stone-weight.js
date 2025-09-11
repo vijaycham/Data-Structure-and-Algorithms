97e02f06d820bcd3344e575b28b1cdc8c510dbf1
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-11 06:29:35 +0530

    Time: 7 ms (27.98%) | Memory: 55 MB (64.29%) - LeetSync

--- Leetcode/Heap/1127-last-stone-weight/last-stone-weight.js
+++ Leetcode/Heap/1127-last-stone-weight/last-stone-weight.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    let pq = new MaxPriorityQueue
+    let pq = new MaxPriorityQueue()
 
     for(let i  = 0; i < stones.length; i++) {
         pq.enqueue(stones[i])
