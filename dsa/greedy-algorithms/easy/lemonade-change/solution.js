fb113be0e3f7281e6571bf723342c7b4d84413d2
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-15 22:26:03 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/greedy-algorithms/easy/lemonade-change?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 115
    - Time: 1.050ms
    - Memory: 52518.00KB

---
+++ dsa/greedy-algorithms/easy/lemonade-change/solution.js
@@ -0,0 +1,25 @@
+// Solution for: 
+class Solution {
+    lemonadeChange(bills) {
+         let wallet = [0, 0]; 
+    for (let i = 0; i < bills.length; ++i) {
+        if (bills[i] === 5) {
+            wallet[0]++;
+        } else if (bills[i] === 10) {
+            wallet[1]++;
+            wallet[0]--;
+        } else { // bill is 20
+            if (wallet[1] > 0) {
+                wallet[1]--;
+                wallet[0]--;
+            } else {
+                wallet[0] -= 3;
+            }
+        }
+        if (wallet[0] < 0) {
+            return false;
+        }
+    }
+    return true;
+    } 
+}
