0953ef05f7f6cedb60b1df05282ff9a564b7fd6e
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-24 04:24:39 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/greedy-algorithms/hard/candy?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 115
    - Time: 1.032ms
    - Memory: 52844.00KB

--- dsa/greedy-algorithms/hard/candy/solution.js
+++ dsa/greedy-algorithms/hard/candy/solution.js
@@ -1,23 +1,32 @@
 // Solution for: 
 class Solution {
-    candy(arr) {
-    let n = arr.length
-    let ltr = Array(n).fill(1)
-    let rtl = Array(n).fill(1)
-    let ans = 0
-    for (let i = 1; i < n; i++) {
-        if (arr[i] > arr[i - 1]) {
-            ltr[i] = ltr[i - 1] + 1
+  candy(arr) {
+     let n = arr.length
+    let ans = n
+    let i = 1
+
+    while (i < n) {
+        while (arr[i] === arr[i - 1]) {
+            i++
+            continue
         }
-    }
-    for (let i = n - 2; i >= 0; i--) {
-        if (arr[i] > arr[i + 1]) {
-            rtl[i] = rtl[i + 1] + 1
+        let up = 0
+        while (i < n && arr[i] > arr[i - 1]) {
+            up++
+            ans += up
+            i++
         }
+        let down = 0
+        while (i < n && arr[i] < arr[i - 1]) {
+            down++
+            ans += down
+            i++
+        }
+        // Removing space 
+        ans = ans - Math.min(up, down)
+
     }
-    for (let i = 0; i < n; i++) {
-        ans = ans + Math.max(ltr[i], rtl[i])
-    }
+
     return ans
-    }
+  }
 }
