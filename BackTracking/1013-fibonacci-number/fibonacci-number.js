a892176cbed5409fc228dcda5316441bd4e6cb94
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-24 08:13:11 +0530

    Time: 48 ms (63.42%) | Memory: 54.3 MB (21.21%) - LeetSync

--- BackTracking/1013-fibonacci-number/fibonacci-number.js
+++ BackTracking/1013-fibonacci-number/fibonacci-number.js
@@ -2,11 +2,11 @@
  * @param {number} n
  * @return {number}
  */
-let store = {}
-var fib = function(n) {
-    if(n<=1) return n
 
-    if(!store[n]) {
-        store[n] = fib(n-1) + fib(n-2)
-    } return store[n]
+var fib = function(n) {
+   let dp = [0,1]
+   for(let i=2; i <=n ; i++) {
+    dp[i]=dp[i-1] + dp[i-2]
+   }
+   return dp[n]
 };
