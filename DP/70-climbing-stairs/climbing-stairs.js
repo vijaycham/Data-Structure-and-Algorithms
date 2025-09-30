f182068b605ec76324770c7e1eebab0ce2e26bf4
VIJAY S <vijaysivakadatcham@gmail.com>
2025-09-30 22:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (5.20%) - LeetSync

---
+++ DP/70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let store = {}
+var climbStairs = function (n) {
+    let dp = [0, 1, 2]
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+}
