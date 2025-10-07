bfde3ed5d3cc94d750aae192620d0fc90c23f438
VIJAY S <vijaysivakadatcham@gmail.com>
2025-10-07 06:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 57.1 MB (35.43%) - LeetSync

--- DP/152-maximum-product-subarray/maximum-product-subarray.js
+++ DP/152-maximum-product-subarray/maximum-product-subarray.js
@@ -4,9 +4,7 @@
  */
 var maxProduct = function (arr) {
     let n = arr.length
-    let maxProdSoFar = arr[0]
-    let minProdSoFar = arr[0]
-    let totalMax = arr[0]
+    let maxProdSoFar = minProdSoFar = totalMax = arr[0]
     for (let i = 1; i < n; i++) {
         let maxProdSoFarCopy = maxProdSoFar
         maxProdSoFar = Math.max(arr[i], maxProdSoFar * arr[i], minProdSoFar * arr[i])
