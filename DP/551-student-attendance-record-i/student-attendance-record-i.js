891326ffd53bb6b90be1d78320f82165fb85f5f6
VIJAY S <vijaysivakadatcham@gmail.com>
2025-10-10 15:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (36.65%) - LeetSync

---
+++ DP/551-student-attendance-record-i/student-attendance-record-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function (s) {
+    if ((s.split('A').length - 1) > 1 || s.includes('LLL')) return false;
+    return true
+
+};
+/**
+ * @param {string} 
+ * @return {boolean}
+ */
