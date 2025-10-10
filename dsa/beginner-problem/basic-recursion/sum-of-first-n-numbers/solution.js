8be7383dcead97d04e6ca502ea21612cff3516bc
VIJAY S <vijaysivakadatcham@gmail.com>
2025-10-10 15:25:40 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/beginner-problem/basic-recursion/sum-of-first-n-numbers?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 114
    - Time: 1.075ms
    - Memory: 52132.00KB

--- dsa/beginner-problem/basic-recursion/sum-of-first-n-numbers/solution.js
+++ dsa/beginner-problem/basic-recursion/sum-of-first-n-numbers/solution.js
@@ -1,56 +1,8 @@
-// Solution for: Count all digits of a number
+// Solution for: 
 class Solution {
+  NnumbersSum(N) {
+    if (N === 0) return 0;
 
-    merge(arr, low, mid, high) {
-        let temp = [];
-        let left = low
-        let right = mid+1;
-
-        while(left <=mid && right <=high) {
-            if(arr[left] <=arr[right]) {
-                temp.push(arr[left]);
-                left ++;
-            } else {
-                temp.push(arr[right]);
-                right ++
-            }
-        }
-        while(left <= mid) {
-            temp.push(arr[left]);
-            left++
-        }
-
-        while (right <=high) {
-            temp.push(arr[right]);
-            right++
-        }
-
-        for (let i= low; i<=high; i++) {
-            arr[i] = temp[i - low]
-        }
-    }
-
-    // helper to perfrom the sort 
-     mergeSortHelper (arr, low, high) {
-        if(low >= high)
-        return;
-
-        let mid = Math.floor((low + high)/2);
-
-        this.mergeSortHelper(arr, low, mid);
-
-        this.mergeSortHelper(arr, mid + 1, high);
-
-        this.merge(arr, low, mid, high)
-     }
-
-
-    mergeSort(nums) {
-
-        let n= nums.length;
-        
-        this.mergeSortHelper(nums, 0, n-1)
-
-        return nums;
-    }
+    return N + this.NnumbersSum(N - 1);
+  }
 }
