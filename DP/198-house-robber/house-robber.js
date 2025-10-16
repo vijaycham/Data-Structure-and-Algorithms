2021f19eb52acbcca7b89a94ae404d095d94fec3
VIJAY S <vijaysivakadatcham@gmail.com>
2025-10-16 22:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (41.17%) - LeetSync

--- DP/198-house-robber/house-robber.js
+++ DP/198-house-robber/house-robber.js
@@ -6,7 +6,7 @@ var rob = function (val) {
     let n = val.length
     if (n == 1) return val[0]
     let p1 = p2=0
-    //let dp = [val[0], Math.max(val[0], val[1])];
+    
     for (let i = 0; i < n; i++) {
         curr = Math.max(val[i] + p2, p1);
         let temp = p1
