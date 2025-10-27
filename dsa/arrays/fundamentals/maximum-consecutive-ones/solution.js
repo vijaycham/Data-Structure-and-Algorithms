1ee8511bb91f5123b7dde719c4350a3a9d3576bd
VIJAY S <vijaysivakadatcham@gmail.com>
2025-10-27 07:57:28 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/arrays/fundamentals/maximum-consecutive-ones?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 116
    - Time: 1.027ms
    - Memory: 53114.00KB

--- dsa/arrays/fundamentals/maximum-consecutive-ones/solution.js
+++ dsa/arrays/fundamentals/maximum-consecutive-ones/solution.js
@@ -7,13 +7,12 @@ class Solution {
     for (let i = 0; i < nums.length; i++) {
       if (nums[i] === 1) {
         count++;
-        max = Math.max(max, count);
       } else {
-        
+        max = Math.max(max, count);
         count = 0;
       }
     }
 
-    return max;
+    return Math.max(max, count);
   }
 }
