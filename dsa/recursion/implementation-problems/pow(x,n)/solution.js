d59c1b4231fa765565e45225297c211241043629
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-02 23:29:45 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/recursion/implementation-problems/pow(x,n)?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 1541
    - Time: 1.045ms
    - Memory: 52400.00KB

--- dsa/recursion/implementation-problems/pow(x,n)/solution.js
+++ dsa/recursion/implementation-problems/pow(x,n)/solution.js
@@ -2,16 +2,13 @@
 class Solution {
   myPower(x, n) {
     if (n === 0) return 1;
-    if (n === 1) return x;
-
-    if (n % 2 === 0) {
-      return this.myPower(x * x, n / 2);
-    }
-    return x * this.myPower(x, n - 1);
+    const half = this.myPower(x, Math.floor(n / 2));
+    return n % 2 === 0 ? half * half : half * half * x;
   }
   myPow(x, n) {
     if (n < 0) {
-      return this.myPower(1 / x, -n);
+      x = 1 / x;
+      n = -n;
     }
     return this.myPower(x, n);
   }
