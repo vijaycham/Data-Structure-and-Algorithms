972d6907fb176babd4ded19c2cdab39d33720c34
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-02 23:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (45.84%) - LeetSync

--- Array/50-powx-n/powx-n.js
+++ Array/50-powx-n/powx-n.js
@@ -3,25 +3,9 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
-    
-    let ans = 1;
-    if (n === 0 || x === 1) return 1;
-
-    if (n < 0) {
-      x = 1 / x;
-      n = -n;
-    }
-
-    while (n > 0) {
-      if (n % 2) {
-        ans = ans * x;
-        n = n - 1;
-      } else {
-        x *= x;
-        n = n / 2;
-      }
-    }
-    return ans;
-
-};
+var myPow = function (x, n) {
+    if (n === 0) return 1;
+    if (n < 0) return myPow(1 / x, -n);
+    const half = myPow(x, Math.floor(n / 2));
+    return n % 2 === 0 ? half * half : half * half * x;
+}
