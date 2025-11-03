96ed3da21795ffa574d454b3473a7587ae232a97
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-03 07:13:49 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/recursion/implementation-problems/generate-parentheses?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 10
    - Time: 1.036ms
    - Memory: 53166.00KB

---
+++ dsa/recursion/implementation-problems/generate-parentheses/solution.js
@@ -0,0 +1,20 @@
+// Solution for: 
+class Solution {
+  generateParenthesis(n) {
+    const results = [];
+    function backtrack(current, open, close){
+      if (current.length === 2 * n) {
+        results.push(current);
+        return;
+      }
+      if (open < n) {
+        backtrack(current + "(", open + 1, close);
+      }
+      if (close < open) {
+        backtrack(current + ")", open, close + 1);
+      }
+    };
+    backtrack("", 0, 0);
+    return results;
+  }
+}
