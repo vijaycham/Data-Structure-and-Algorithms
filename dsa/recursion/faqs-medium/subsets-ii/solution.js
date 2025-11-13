cd63ade007bf0c8a973674e80e03d8273b92418b
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-13 22:16:49 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/recursion/faqs-medium/subsets-ii?tab=editorial
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 857
    - Time: 1.601ms
    - Memory: 52416.00KB

---
+++ dsa/recursion/faqs-medium/subsets-ii/solution.js
@@ -0,0 +1,17 @@
+// Solution for: 
+class Solution {
+  subsetSums(nums) {
+    function func(ind, sum, nums, ans) {
+      if (ind === nums.length) {
+        ans.push(sum);
+        return;
+      }
+      func(ind + 1, sum + nums[ind], nums, ans);
+      func(ind + 1, sum, nums, ans);
+    }
+
+    let ans = [];
+    func(0, 0, nums, ans);
+    return ans;
+  }
+}
