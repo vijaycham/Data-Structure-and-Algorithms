ac0f3732792e1a1195d12479529d3872e6575b5d
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-20 22:45:58 +0530

    Solved:
    Problem Link: https://takeuforward.org/plus/dsa/sorting/algorithms/selection-sort?tab=submissions
    
    Description:
    
    
    Stats:
    - Success: true
    - Test Cases: 123
    - Time: 1.039ms
    - Memory: 52470.00KB

--- dsa/sorting/algorithms/selection-sort/solution.js
+++ dsa/sorting/algorithms/selection-sort/solution.js
@@ -1,17 +1,18 @@
 // Solution for: 
 class Solution {
-    selectionSort(nums) {
-        let n = nums.length
-        for(let i= 0 ; i < n-1; i++) {
-            let minIndex = i 
-            for (let j = i+1 ; j < n; j++ )
-            {
-                if(nums[j] < nums[minIndex]) 
-                 minIndex = j
-            }
-            if(minIndex !== i) {
-                [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]]
-            }
-        } return nums
+  selectionSort(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      let minIndex = i;
+
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[j] < nums[minIndex]) {
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) {
+        [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
+      }
     }
+    return nums;
+  }
 }
