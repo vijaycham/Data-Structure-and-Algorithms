e90ff729d256282eee12b296d5e878903f19bb7e
VIJAY S <vijaysivakadatcham@gmail.com>
2025-11-26 23:36:40 +0530

    Time: 44 ms (94.56%) | Memory: 57.2 MB (62.42%) - LeetSync

---
+++ Array/133-clone-graph/clone-graph.js
@@ -0,0 +1,35 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+
+ var cloneGraph = function(root) {
+    if (!root) return null;
+
+    const q = [root];
+    const visited = new Map();
+    const cloneRoot = new Node(root.val);
+    visited.set(root, cloneRoot);
+
+    while (q.length) {
+        const curr = q.shift();
+        const cloneCurr = visited.get(curr);
+
+        for (const n of curr.neighbors) {
+            if (!visited.has(n)) {
+                visited.set(n, new Node(n.val));
+                q.push(n);
+            }
+            cloneCurr.neighbors.push(visited.get(n));
+        }
+    }
+    return cloneRoot;
+};       
