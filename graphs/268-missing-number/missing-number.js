8e39018550f2945e29c9d7e41e6983c8b6dcc8e4
VIJAY S <vijaysivakadatcham@gmail.com>
2025-12-16 09:41:17 +0530

    Time: 1 ms (66.49%) | Memory: 56.6 MB (54.85%) - LeetSync

--- graphs/268-missing-number/missing-number.js
+++ graphs/268-missing-number/missing-number.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    let n = nums.length
-    let sum = (n*(n+1))/2
-    let givenSum = 0
-    for (let i = 0; i < n; i++) {
-        givenSum += nums[i]
+    let xor = nums.length
+    for (let i = 0; i < nums.length; i++) {
+        xor = xor ^ i ^ nums[i]
     }
-    return sum - givenSum
+    return xor
 };
