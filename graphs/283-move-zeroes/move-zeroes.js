7a18b85a3dcb8bbfc4cc7fc4e9265f65f6b9ffe3
VIJAY S <vijaysivakadatcham@gmail.com>
2025-12-16 08:13:51 +0530

    Time: 1 ms (75.30%) | Memory: 58.6 MB (89.75%) - LeetSync

--- graphs/283-move-zeroes/move-zeroes.js
+++ graphs/283-move-zeroes/move-zeroes.js
@@ -2,15 +2,17 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
+var moveZeroes = function(nums) {
+    let n = nums.length
+    let j = 0
 
-   let j = 0 
-   for(let i = 0; i < nums.length; i++) {
-    if(nums[i] !== 0 ) {
-        nums[j++]= nums[i]
+    for(let i = 0; i<n;i++) {
+        if(nums[i] !== 0){
+            nums[j++] = nums[i]
+        }
     }
-   }
-   while(j < nums.length) {
-     nums[j++] = 0
-   }
+
+    while(j < n) {
+        nums[j++] = 0
+    }
 };
