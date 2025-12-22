53d56500867b348006eac8601855b2cc79d3ac93
VIJAY S <vijaysivakadatcham@gmail.com>
2025-12-22 22:48:21 +0530

    Time: 1 ms (55.44%) | Memory: 58.1 MB (52.02%) - LeetSync

---
+++ graphs/2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let ans = []
+
+    for(let i = 0; i< nums.length; i++) {
+        ans.push(nums[i])
+    }
+    for(let i = 0; i< nums.length; i++) {
+        ans.push(nums[i])
+    }
+    return ans
+};
