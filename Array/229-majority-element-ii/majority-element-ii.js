d2dc345921afe37258660046cfa3d228168bd9a7
VIJAY S <vijaysivakadatcham@gmail.com>
2025-12-26 08:19:22 +0530

    Time: 5 ms (73.04%) | Memory: 60 MB (46.23%) - LeetSync

--- Array/229-majority-element-ii/majority-element-ii.js
+++ Array/229-majority-element-ii/majority-element-ii.js
@@ -4,24 +4,24 @@
  */
 var majorityElement = function (nums) {
 
-    let n = nums.length;
+     let n = nums.length;
     let result = [];
+    let map = new Map();
+    let mini = Math.floor(n / 3) + 1;
 
     for (let i = 0; i < n; i++) {
-        if (result.includes(nums[i])) continue;
+      let num = nums[i];
+      if (!map.has(num)) {
+        map.set(num, 0);
+      }
+      map.set(num, map.get(num) + 1);
 
-        let count = 0;
-        for (let j = 0; j < n; j++) {
-            if (nums[j] === nums[i]) {
-                count++;
-            }
-        }
-        if (count > Math.floor(n / 3)) {
-            result.push(nums[i]);
-        }
-        if (result.length === 2) {
-            break;
-        }
+      if (map.get(num) === mini) {
+        result.push(num);
+      }
+      if (result.length === 2) {
+        break;
+      }
     }
     return result;
 }
