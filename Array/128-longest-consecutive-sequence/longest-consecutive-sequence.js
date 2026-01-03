178a44245c57a1fec1da18bb885537b4477d0d6d
VIJAY S <vijaysivakadatcham@gmail.com>
2026-01-03 22:57:15 +0530

    Time: 35 ms (82.87%) | Memory: 67.5 MB (90.69%) - LeetSync

--- Array/128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ Array/128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -4,33 +4,36 @@
  */
 var longestConsecutive = function (nums) {
     let n = nums.length;
-    // If the array is empty
-    if (n === 0) return 0;
-    // Initialize the longest sequence length
-    let longest = 1;
-    let st = new Set();
-    // Put all the array elements into the set
-    for (let i = 0; i < n; i++) {
-        st.add(nums[i]);
-    }
-    // Traverse the set to find the longest sequence
-    for (let it of st) {
-        // Check if 'it' is a starting number of a sequence
-        if (!st.has(it - 1)) {
-            // Initialize the count of the current sequence
-            let cnt = 1;
-            // Starting element of the sequence
-            let x = it;
-            // Find consecutive numbers in the set
-            while (st.has(x + 1)) {
-                // Move to the next element in the sequence
-                x = x + 1;
-                // Increment the count of the sequence
-                cnt = cnt + 1;
+
+        // Return 0 if array is empty
+        if (n === 0) return 0; 
+
+        nums.sort((a, b) => a - b);
+
+        // Track last smaller element
+        let lastSmaller = -Infinity; 
+        // Count current sequence length
+        let cnt = 0; 
+        // Track longest sequence length
+        let longest = 1; 
+
+        for (let i = 0; i < n; i++) {
+            // If consecutive number exists
+            if (nums[i] - 1 === lastSmaller) {
+                // Increment sequence count
+                cnt += 1; 
+                // Update last smaller element
+                lastSmaller = nums[i]; 
+            } 
+            // If consecutive number doesn't exist
+            else if (nums[i] !== lastSmaller) {
+                // Reset count for new sequence
+                cnt = 1; 
+                // Update last smaller element
+                lastSmaller = nums[i]; 
             }
-            // Update the longest sequence length
-            longest = Math.max(longest, cnt);
+            // Update longest if needed
+            longest = Math.max(longest, cnt); 
         }
-    }
-    return longest;
+        return longest;
 };
