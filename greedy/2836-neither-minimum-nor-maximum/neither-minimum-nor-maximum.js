dbc4817a871b1f494409e808abdb959f42bf7f71
VIJAY S <vijaysivakadatcham@gmail.com>
2026-03-15 13:33:32 +0530

    Time: 4 ms (91.60%) | Memory: 66.8 MB (6.72%) - LeetSync

--- greedy/2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
+++ greedy/2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
@@ -2,20 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNonMinOrMax = function(nums) {
+var findNonMinOrMax = function (nums) {
     let n = nums.length
-    if(n <= 2) return -1
+    if (n <= 2) return -1
     let min = Infinity
     let max = -Infinity
-    for(let i = 0; i < n; i++) {
-        if(nums[i] > max) {
-            max = nums[i]        }
-        if(nums[i] < min) {
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > max) {
+            max = nums[i]
+        }
+        if (nums[i] < min) {
             min = nums[i]
-        }    
-    } 
-    for( let i =0; i< n; i++) {
-        if(nums[i] !== max && nums[i] !== min) {
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== max && nums[i] !== min) {
             return nums[i]
         }
     }
