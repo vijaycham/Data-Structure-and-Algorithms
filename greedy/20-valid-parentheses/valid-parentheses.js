e15da6e3b21d87b3f6b4cbd1997d178bc040f1a2
VIJAY S <vijaysivakadatcham@gmail.com>
2026-03-30 23:59:23 +0530

    Time: 6 ms (21.73%) | Memory: 55.3 MB (69.09%) - LeetSync

--- greedy/20-valid-parentheses/valid-parentheses.js
+++ greedy/20-valid-parentheses/valid-parentheses.js
@@ -4,15 +4,20 @@
  */
 var isValid = function (s) {
     let stack = []
+
+    let map = {
+        "[": "]",
+        "{": "}",
+        "(": ")"
+    }
+
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "[" || s[i] === "(" || s[i] === "{") {
+        if (map[s[i]]) {
             stack.push(s[i])
         } else {
             let top = stack.pop()
             if (!top
-                || (top === "[" && s[i] != "]")
-                || (top === "(" && s[i] != ")")
-                || (top === "{" && s[i] != "}")) {
+                || (s[i]) != map[top]) {
                 return false
             }
         }
