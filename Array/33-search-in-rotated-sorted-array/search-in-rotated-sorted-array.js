081018d9c75ba09f187759d6347bd35f159b704f
VIJAY S <vijaysivakadatcham@gmail.com>
2026-05-22 21:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (47.41%) - LeetSync

---
+++ Array/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0
+    let n = nums.length
+    right = n - 1
+
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2)
+        if (target === nums[mid]) {
+            return mid
+        } else if (nums[mid] >= nums[left]) {
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1
+            } else left = mid + 1
+        } else if (nums[right] >= nums[mid]) {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+
+    } return -1
+};
